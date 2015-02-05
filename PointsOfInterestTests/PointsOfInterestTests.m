d2db99ec8d3328c53e30ce38b71d84d8933c5339
Peter Shultz <peterkshultz@verizon.net>
2015-02-04 19:51:59 -0500

    Initial commit

---
+++ PointsOfInterestTests/PointsOfInterestTests.m
@@ -0,0 +1,40 @@
+//
+//  PointsOfInterestTests.m
+//  PointsOfInterestTests
+//
+//  Created by Peter Shultz on 2/4/15.
+//  Copyright (c) 2015 Peter Shultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PointsOfInterestTests : XCTestCase
+
+@end
+
+@implementation PointsOfInterestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
